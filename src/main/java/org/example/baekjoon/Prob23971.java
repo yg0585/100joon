a0e6a33e1cc8210d84d3cf4c225aeab441277b50
yg0585 <yg0585@naver.com>
2023-03-02 19:06:39 +0900

    23971

---
+++ src/main/java/org/example/baekjoon/Prob23971.java
@@ -0,0 +1,27 @@
+package org.example.baekjoon;
+
+import java.util.Scanner;
+
+public class Prob23971 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int height = scanner.nextInt();
+		int width = scanner.nextInt();
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		int xc = 0;
+		int yc = 0;
+
+		int x = 1, y = 1;
+		while (x <= width) {
+			xc++;
+			x += m + 1;
+		}
+
+		while (y <= height) {
+			yc++;
+			y += n + 1;
+		}
+		System.out.println(xc * yc);
+	}
+}
