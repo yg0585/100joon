3f0efad2574d5f751586ba3e75cadc0c64b70bfb
yg0585 <yg0585@naver.com>
2023-04-12 14:06:36 +0900

    solve : 프로그래머스 정수 삼각형

---
+++ src/main/java/org/example/programmers/Triangle.java
@@ -0,0 +1,26 @@
+package org.example.programmers;
+
+public class Triangle {
+	public int solution(int[][] triangle) {
+		int max = 0;
+
+		for (int i = 1; i < triangle.length; i++) {
+			for (int j = 0; j < triangle[i].length; j++) {
+				if (j == 0) {
+					triangle[i][j] += triangle[i - 1][j];
+				}
+				else if (j == triangle[i].length - 1) {
+					triangle[i][j] += triangle[i - 1][j - 1];
+				}
+				else
+					triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+
+				if (triangle[i][j] > max)
+					max = triangle[i][j];
+			}
+		}
+
+		return max;
+	}
+
+}
